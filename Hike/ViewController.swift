a5fdef01c13e744aed0b585823dbd9f0cc30559d
Valeriu POPA <vapopa@pentalog.fr>
2018-09-30 16:23:24 +0300

    Initial Commit

---
+++ Hike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hike
+//
+//  Created by Valeriu POPA on 9/30/18.
+//  Copyright © 2018 Valeriu Popa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
